80f644b3edb0dcfc487bcf36cd6d1cdfd62b4182
tipi <tipi@nc-ass-vip.sdv.fr>
2024-06-03 17:41:02 +0200

    header ongoing

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,15 @@
+import './Hero.scss'
+const Hero = () => {
+  return (
+    <div className="hero">
+      <section className="hero-content">
+        <p className="subtitle">No fees.</p>
+        <p className="subtitle">No minimum deposit.</p>
+        <p className="subtitle">High interest rates.</p>
+        <p className="text">Open a savings account with Argent Bank today!</p>
+      </section>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+import Header from "./header/Header";
+import Hero from "./hero/Hero";
+
+export { Header, Hero }
